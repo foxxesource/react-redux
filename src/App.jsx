a75368fd6c5bb9af3a538657ee47ef5d9e9da247
foxxesource <bintang.duinata31@gmail.com>
2025-06-21 13:07:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './store/counter/actions';
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const count = useSelector(state => state.count)
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h1>Count : {count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/store/counter/actions.js
@@ -0,0 +1,3 @@
+export const increment = () => {{type:'increment'}}
+
+export const decrement = () => {{type:'decrement'}}
